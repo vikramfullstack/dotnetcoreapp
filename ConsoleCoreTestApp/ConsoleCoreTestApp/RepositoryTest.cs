db1d4928f4588f67d7d909a3a301c13adb587f1f
vikramfullstack <vikramfullstack247@gmail.com>
2022-02-09 10:35:23 +0530

    Dotnet Test Project Files added

---
+++ ConsoleCoreTestApp/ConsoleCoreTestApp/RepositoryTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace ConsoleCoreTestApp
+{
+    public class RepositoryTest
+    {
+        [Fact]
+        public void TestToCheckXunitFirstTestCase()
+        {
+            int expectedResult = 10;
+            //Act
+            int actualResultFromDevCode = 10;
+            //Assert
+            Assert.Equal(expectedResult, actualResultFromDevCode);
+        }
+    }
+}
